0208b08f06f2c23280de26fabaf60e1a1933eac7
Mheyrie <omota4fidelity@gmail.com>
2023-11-03 08:46:39 +0100

    doen

--- lib/scraper.py
+++ lib/scraper.py
@@ -4,3 +4,11 @@ import requests
 
 headers = {'user-agent': 'my-app/0.0.1'}
 html = requests.get("https://flatironschool.com/", headers=headers)
+
+doc = BeautifulSoup(html.text, 'html.parser')
+
+# print(doc.select('.heading-primary')[0].contents)
+courses = doc.select('.heading-60-black')
+
+for course in courses: 
+  print(course.contents[0].strip())
